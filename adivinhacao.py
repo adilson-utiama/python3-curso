a27e765e8c3401ec81227a93846d063819f0a3da
Adilson S. Utiama <adilson.utiama@gmail.com>
2017-04-30 20:12:42 -0300

    Laço com while

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,22 +3,28 @@ print("Bem vindo ao jogo de adivinhação")
 print("********************************")
 
 numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1
 
-chute_str = input("Digite o numero: ")
-print("Você digitou " , chute_str)
-chute = int(chute_str)
+while(rodada <= total_de_tentativas):
+    print("Tentativa", rodada, "de", total_de_tentativas)
+    chute_str = input("Digite o numero: ")
+    print("Você digitou " , chute_str)
+    chute = int(chute_str)
 
-acertou = chute == numero_secreto
-maior   = chute > numero_secreto
-menor   = chute < numero_secreto
+    acertou = (chute == numero_secreto)
+    maior   = chute > numero_secreto
+    menor   = chute < numero_secreto
 
-# Condicional em Python
-if(acertou):
-    print("Você acertou!") #obrigatório identação com 4 espacos(tab)
-else:
-    if(maior):
-        print("Você errou! O seu chute foi maior que o numero secreto.")
-    elif(menor):
-        print("Você errou! O seu chute foi menor que o numero secreto.")
+    # Condicional em Python
+    if(acertou):
+        print("Você acertou!") #obrigatório identação com 4 espacos(tab)
+    else:
+        if(maior):
+            print("Você errou! O seu chute foi maior que o numero secreto.")
+        elif(menor): #equivalente a elseif em java
+            print("Você errou! O seu chute foi menor que o numero secreto.")
+
+    rodada += 1
 
 print("Fim do Jogo")
