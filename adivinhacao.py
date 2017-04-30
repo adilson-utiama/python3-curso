8029da366b7e457687f5c14d913c0135003bcdc2
Adilson S. Utiama <adilson.utiama@gmail.com>
2017-04-30 19:19:33 -0300

    Condicional if..else..elif

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,1 +1,24 @@
-print("Ola Mundo")
+print("********************************")
+print("Bem vindo ao jogo de adivinhação")
+print("********************************")
+
+numero_secreto = 42
+
+chute_str = input("Digite o numero: ")
+print("Você digitou " , chute_str)
+chute = int(chute_str)
+
+acertou = chute == numero_secreto
+maior   = chute > numero_secreto
+menor   = chute < numero_secreto
+
+# Condicional em Python
+if(acertou):
+    print("Você acertou!") #obrigatório identação com 4 espacos(tab)
+else:
+    if(maior):
+        print("Você errou! O seu chute foi maior que o numero secreto.")
+    elif(menor):
+        print("Você errou! O seu chute foi menor que o numero secreto.")
+
+print("Fim do Jogo")
