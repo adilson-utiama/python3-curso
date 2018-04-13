c997159e561c9db214ac76b4fd9e814db55f1597
Adilson S. Utiama <adilson.utiama@gmail.com>
2018-04-13 16:34:14 -0300

    Getters e Setters

--- orientacao_objeto/conta.py
+++ orientacao_objeto/conta.py
@@ -19,3 +19,18 @@ class Conta:
     def transfere(self, valor, destino):
         self.saca(valor)
         destino.deposita(valor)
+
+    def get_titular(self):
+        return self.__titular
+
+    def get_numero(self):
+        return self.__numero
+
+    def get_saldo(self):
+        return self.__saldo
+
+    def get_limite(self):
+        return self.__limite
+
+    def set_limite(self, novo_limite):
+        self.__limite = novo_limite
